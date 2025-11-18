3ce5b224b6ed644d9374733659221e1e00af98e7
Lokesh-Reddy-Kambham <kambhamlokeshreddy@gmail.com>
2025-11-19 00:43:25 +0530

    Greatest of two Numbers

---
+++ PrepInsta-Challenges/greatest-of-two-numbers.py
@@ -0,0 +1,8 @@
+n1 = int(input("Enter Number:"))
+n2 = int(input("Enter Number:"))
+if n1>n2:
+    print(f"{n1} is greater than {n2}")
+elif n2>n1:
+    print(f"{n2} is greater than {n1}")
+else:
+    print(f"{n1} and {n2} are equal")
